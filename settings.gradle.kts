94720ebe095acb55a662cce44ab4fc5bfad8bf56
“hongwei.jiang” <hongwei.jiang@aifenlei.com>
2026-06-26 17:27:39 +0800

    fix: reorder repositories - google/mavenCentral first for CI compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,22 @@
 pluginManagement {
     repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
         maven("https://maven.aliyun.com/repository/gradle-plugin")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/central")
         maven("https://maven.aliyun.com/repository/public")
-        google()
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositories {
+        google()
+        mavenCentral()
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/central")
         maven("https://maven.aliyun.com/repository/public")
-        google()
-        mavenCentral()
     }
 }
 
